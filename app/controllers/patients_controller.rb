98f3c7ce6674771229a84c56ca17dbf0c1840a30
Emma Scanlon <emmascanlon4@gmail.com>
2019-02-26 14:20:10 -0700

    add doctors and patients models and controllers

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,47 @@
+class PatientsController < ApplicationController
+  before_action :set_course, only: [:show, :destroy, :edit, :update]
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.create(patient_params)
+    if @patient.save
+      redirect_to @patient
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @patient.update(patient_params)
+      redirect_to patients_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @patient.destroy
+    redirect_to patients_path
+  end
+
+  private
+  def set_patient
+    @patient = Patient.find(params[:id])
+end
+
+def patient_params
+  params.require(:course).permit.(:name)
+end
+end
